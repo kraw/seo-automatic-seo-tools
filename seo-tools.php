fd4543ddbfd169130133ab5f9a3bbd7bfee68a06
Dzynit <Dzynit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-22 16:40:29 +0000

    git-svn-id: https://plugins.svn.wordpress.org/seo-automatic-seo-tools/tags/3.1.4@413815 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ seo-tools.php
@@ -0,0 +1,388 @@
+<?php
+/*
+Plugin Name: SEO Tools by SEO Automatic 
+Plugin URI: http://www.seoautomatic.com/plugins/unique-seo-tools/
+Description: Unique SEO tools for your visitors or employees to perform repetetive tasks efficiently, or to otherwise save time.  Created by Search Commander, Inc. for free distribution. <br />See <a href="admin.php?page=seo-automatic-options">SEO Automatic</a> > <a href="?page=seo-tools/settings.php">SEO Tools</a> for options. 
+Version: 3.1.4
+Author: cyber49
+Author URI: http://www.seoautomatic.com/plugins/unique-seo-tools/
+*/
+
+//make sure I change
+
+//bulk url checker
+function cleanData(&$str) { 
+	$str = preg_replace("/\t/", "\\t", $str); 
+	$str = preg_replace("/\n/", "\\n", $str);
+}
+
+    function getHttpResponseCode($url)
+    {
+        $ch = @curl_init($url);
+        @curl_setopt($ch, CURLOPT_HEADER, TRUE);
+        @curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+        @curl_setopt($ch, CURLOPT_FOLLOWLOCATION, FALSE);
+        @curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $status = array();
+        $response = @curl_exec($ch);
+        preg_match('/HTTP\/.* ([0-9]+) .*/', $response, $status);
+        if ($status[1] == '301'){
+	        preg_match('/location: ([^\s]+)/i', $response, $redirect);
+			$statusredirect = $status[1].':|:'.$redirect[1];
+		return $statusredirect;	
+	
+		} elseif ($response == ''){
+		return 'NoResponse';
+
+		} else 
+		return trim($status[1]);
+    }
+
+function sc_404_header_scripts() {
+	$sc_plugin_dir =  get_option('siteurl').'/wp-content/plugins/seo-automatic-seo-tools/sc-bulk-url-checker/';
+	echo '<link rel="stylesheet" href="'.$sc_plugin_dir.'tablesorter/themes/blue/style.css" type="text/css" id="bulkurl" media="print, projection, screen" />';
+}
+
+add_action('wp_head', 'sc_404_header_scripts');
+
+function sc_get_404_page(){
+	ob_start();
+		require_once(ABSPATH.PLUGINDIR.'/seo-automatic-seo-tools/sc-bulk-url-checker/index.php');
+		$bulkpage = new Bulk404Object;
+		$bulkpage->sc_404_page();
+	$return = ob_get_contents();
+	ob_end_clean();
+	return $return;
+}
+
+
+function sc_add_404_page(){
+	$seotools = '<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><small><a href="http://www.seoautomatic.com/free-tools/bulk-url-checker/" target="_blank">This Bulk Url Checker was provied by SEO Automatic</a></small>';
+	return sc_get_404_page().$seotools;
+}
+
+add_shortcode('urlchecker', 'sc_add_404_page');
+
+
+//feedcommander
+function sc_get_feedcommander(){
+ob_start();
+    require_once(ABSPATH.'wp-content/plugins/seo-automatic-seo-tools/feedcommander/'.'feedcommander.php');
+	$bulkpage = new feedcommander_free;
+	$bulkpage->sc_feedcommander_free();
+$return = ob_get_contents();
+ob_end_clean();
+return $return;
+}
+
+
+function sc_add_feedcommander(){
+	$seotools = '<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><small><a href="http://www.seoautomatic.com/free-tools/feedcommander/" target="_blank">This Feed Commander was provided by SEO Automatic</a></small>';
+	return sc_get_feedcommander().$seotools;
+}
+
+add_shortcode('feedcommander', 'sc_add_feedcommander');
+
+
+//link variance
+class linkvariance {
+	function sc_linkvariance(){
+		if ($_REQUEST['run'] == "yes") {
+			
+			extract($_REQUEST);
+			if ($nofollow == "ON") { $rel = ' rel="nofollow"'; }
+			if ($newtab == "ON") { $newwin = ' target="_blank"'; }
+			$keywords = "";
+			 
+			$list1 = preg_replace('/\r\n|\r/', ',', $input1);
+			$list2 = preg_replace('/\r\n|\r/', ',', $input2);
+			$list1 = explode( ",", $list1 );
+			$list2 = explode( ",", $list2 );
+		 
+			if ($novary == "ON") { 
+				$nvx = 0;
+				$nv = count($list2);
+				while ($nvx < $nv):
+					$keywords = $keywords ."\n". '<a href="' . trim($list1[$nvx]) . '"' . $rel . $newwin . '>' . trim($list2[$nvx]) . '</a>';
+					$nvx++;
+				endwhile;	
+			} else {		 
+			   foreach( $list1 as $word1 ) {		 
+				  foreach( $list2 as $word2 ) {		 		 
+						$keywords = $keywords ."\n". '<a href="' . trim($word1) . '"' . $rel . $newwin . '>' . trim($word2) . '</a>';
+					 }			 
+				}	
+			}
+		}
+?>
+
+	<form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>" name="linkvariance">
+	<input type="hidden" name="run" value="yes" />
+	  <div align="center">
+		
+		<table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" width="482">
+		  <tr>
+			<td width="100" align="center" valign="top">URL List</td>
+			<td width="10" align="center" valign="top">&nbsp;</td>
+			<td width="100" align="center" valign="top">Anchor Text</td>
+		 </tr>
+		  <tr>
+			<td width="100" align="center" valign="top">
+			<textarea rows="23" id="input1" name="input1" cols="24"><?php echo $input1;?></textarea></td>
+			<td width="10" align="center" valign="top">&nbsp;</td>
+			<td width="100" align="center" valign="top">
+			<textarea rows="23" id="input2" name="input2" cols="24"><?php echo $input2;?></textarea></td>
+		  </tr>
+		  <tr>
+			<td colspan="3" align="center" valign="top"><br />
+			<input type="checkbox" name="nofollow" value="ON" checked> Add nofollow  &nbsp;<input type="checkbox" name="newtab" value="ON" checked> Open Links in New Window<br /><input type="checkbox" name="novary" value="ON"> Do not vary phrases <font size="1">(This will create just one .html link per phrase, retaining the order in which they're entered.)</font>
+			<br /><input type="submit" value="Process" name="submit"> <input type="reset" value="Reset" name="B2"></td>
+		  </tr>
+		</table>
+		
+	  </div>
+	  <p align="center"><textarea rows="23" id="links" name="links" cols="59"><?php echo $keywords;?></textarea></p>
+	  <p>&nbsp;</p>
+	</form>
+<?php
+	}
+}
+
+function sc_get_linkvariance(){
+ob_start();
+	$bulkpage = new linkvariance;
+	$bulkpage->sc_linkvariance();
+$keywords = ob_get_contents();
+ob_end_clean();
+return $keywords;
+}
+
+
+function sc_add_linkvariance(){
+	$seotools = '<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><small><a href="http://www.seoautomatic.com/free-tools/link-variance/" target="_blank">This Link Variance Tool was provided by SEO Automatic</a></small>';
+	return sc_get_linkvariance().$seotools;
+}
+
+add_shortcode('link-variance', 'sc_add_linkvariance');
+
+
+//keyword marriage
+class keywordmarriage {
+function sc_keywordmarriage(){
+	if ($_REQUEST['run'] == "yes") {
+	$_REQUEST['keywords'] = preg_replace('/\r\n|\r/', ",", $_REQUEST['keywords']);
+	$keywords = explode(',', $_REQUEST['keywords']);
+	$_REQUEST['addkeywords'] = preg_replace('/\r\n|\r/', ",", $_REQUEST['addkeywords']);
+	$addkeywords = explode(',', $_REQUEST['addkeywords']);
+	$_REQUEST['addkeywords2'] = preg_replace('/\r\n|\r/', ",", $_REQUEST['addkeywords2']);
+	$addkeywords2 = explode(',', $_REQUEST['addkeywords2']);
+	$_REQUEST['addkeywords3'] = preg_replace('/\r\n|\r/', ",", $_REQUEST['addkeywords3']);
+	$addkeywords3 = explode(',', $_REQUEST['addkeywords3']);
+
+	if ($_REQUEST['links'] == "ON") { $space = ""; } else { $space = " "; }
+
+	for ($i = 0; $i < count($keywords); $i++) { 
+		if ($keywords[$i] == '*keep~going*') {
+		} else {
+			for ($j = 0; $j < count($addkeywords); $j++) {
+				if ($addkeywords[$j] == '*keep~going*') {
+				} else {
+					for ($k = 0; $k < count($addkeywords2); $k++) {
+						if ($addkeywords2[$k] == '*keep~going*') {
+						} else {
+							for ($l = 0; $l < count($addkeywords3); $l++) {
+								if ($addkeywords3[$l] == '*keep~going*') {
+									} else {
+										if (($_REQUEST['addkeywords2'] != '') && ($_REQUEST['addkeywords3'] == '')) {
+											$result = $result . $keywords[$i] . $space . $addkeywords[$j] . $space . $addkeywords2[$k] . "\n";
+											if ($_REQUEST['phrase'] == "ON") {
+												$result = $result . "\"" . $keywords[$i] . $space . $addkeywords[$j] . $space . $addkeywords2[$k] . "\"\n";
+											}
+											if ($_REQUEST['exact'] == "ON") {
+												$result = $result . "[" . $keywords[$i] . $space . $addkeywords[$j] . $space . $addkeywords2[$k] . "]\n";
+											}
+										} elseif (($_REQUEST['addkeywords2'] == '') && ($_REQUEST['addkeywords3'] != '')) {
+											$result = $result . $keywords[$i] . $space . $addkeywords[$j] . $space . $addkeywords3[$l] . "\n";
+											if ($_REQUEST['phrase'] == "ON") {
+												$result = $result . "\"" . $keywords[$i] . $space . $addkeywords[$j] . $space . $addkeywords3[$l] . "\"\n";
+											}
+											if ($_REQUEST['exact'] == "ON") {
+												$result = $result . "[". $keywords[$i] . $space . $addkeywords[$j] . $space . $addkeywords3[$l] . "]\n";
+											}
+										} elseif (($_REQUEST['addkeywords2'] == '') && ($_REQUEST['addkeywords3'] == '')) {
+											$result = $result . $keywords[$i] . $space . $addkeywords[$j] . "\n";
+											if ($_REQUEST['phrase'] == "ON") {
+												$result = $result . "\"" . $keywords[$i] . $space . $addkeywords[$j] . "\"\n";
+											}
+											if ($_REQUEST['exact'] == "ON") {
+												$result = $result . "[" . $keywords[$i] . $space . $addkeywords[$j] . "]\n";
+											}
+										} else {
+										$result = $result . $keywords[$i] . $space . $addkeywords[$j] . $space . $addkeywords2[$k] . $space . $addkeywords3[$l] . "\n";
+										if ($_REQUEST['phrase'] == "ON") {
+											$result = $result . "\"" . $keywords[$i] . $space . $addkeywords[$j] . $space . $addkeywords2[$k] . $space . $addkeywords3[$l] . "\"\n";
+										}
+										if ($_REQUEST['exact'] == "ON") {
+											$result = $result . "[" . $keywords[$i] . $space . $addkeywords[$j] . $space . $addkeywords2[$k] . $space . $addkeywords3[$l] . "]\n";
+										}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	$result = substr_replace($result,"",-1);
+	$result = str_replace("\\","", $result); 
+	if ( $result{0} == " " ) { $result = substr($result, 1); }
+						}
+	?>
+<script type="text/javascript">
+function uncheck(){
+	var x=document.forms.keywordmarriage
+	x['phrase'].checked=false
+	x['phrase'].disabled= !x['phrase'].disabled
+	var y=document.forms.keywordmarriage
+	x['exact'].checked=false
+	x['exact'].disabled= !x['exact'].disabled
+}
+</script>
+	<form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>" name="keywordmarriage">
+	<input type="hidden" name="run" value="yes" />
+	  <div align="center">
+		
+		<table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" width="454" height="1017">
+		  <tr>
+			<td width="216" align="center" valign="top">Primary Keyword Phrases</td>
+			<td width="24" align="center" valign="top">&nbsp;</td>
+			<td width="213" align="center" valign="top">Desired Variables (city, state etc.)</td>
+			<td width="4" align="center" valign="top">&nbsp;</td>
+		 </tr>
+		  <tr>
+			<td width="216" align="center" valign="top" height="324">
+			<textarea rows="20" id="keywords" name="keywords" cols="24"></textarea></td>
+			<td width="24" align="center" valign="top" height="324">&nbsp;</td>
+			<td width="213" align="center" valign="top" height="324">
+			<textarea rows="20" name="addkeywords" cols="24"></textarea></td>
+			<td width="4" align="center" valign="top" height="324">&nbsp;</td>
+		  </tr>
+		  <tr>
+			<td width="216" align="center" valign="top" height="34">
+			&nbsp;</td>
+			<td width="24" align="center" valign="top" height="34">&nbsp;</td>
+			<td width="213" align="center" valign="top" height="34">
+			&nbsp;</td>
+			<td width="4" align="center" valign="top" height="34">&nbsp;</td>
+		  </tr>
+		  <tr>
+			<td width="216" align="center" valign="top">Desired Variables (city, state etc.)</td>
+			<td width="24" align="center" valign="top">&nbsp;</td>
+			<td width="213" align="center" valign="top">Desired Variables (city, state etc.)</td>
+			<td width="4" align="center" valign="top">&nbsp;</td>
+		  </tr>
+		  <tr>
+			<td width="216" align="center" valign="top" height="324">
+			<textarea rows="20" name="addkeywords2" cols="24"></textarea></td>
+			<td width="24" align="center" valign="top" height="324">&nbsp;</td>
+			<td width="213" align="center" valign="top" height="324">
+			<textarea rows="20" name="addkeywords3" cols="24"></textarea></td>
+			<td width="4" align="center" valign="top" height="324">&nbsp;</td>
+		  </tr>
+		  <tr>
+			<td colspan="4" align="left" valign="top" height="221" width="457"><br />
+<b>If you're using this for Google Ad Words, you likely want to leave these boxes checked.</b><br />
+			<input type="checkbox" name="phrase" value="ON" checked> Add Phrase Match &nbsp;<input type="checkbox" name="exact" value="ON" checked> Add Exact Match <br /><br />
+			<input type="checkbox" name="links" value="ON" onclick="uncheck()"> &nbsp;Checking this box CHANGES this tool, so it will generate EXACTLY what you input. Do do not use with the Adwords match types. 
+<br /><br />
+This option KEEPS any spaces or other characters you may add, like pipes, spaces etc. and will NOT add its own spaces for use in Adwords. 
+<br />
+ <br /><input type="submit" value="Create List of Keywords" name="submit"> <input type="reset" value="Reset" name="B2"></td>
+		  </tr>
+		</table>
+		
+	  </div>
+	  <p align="center"><textarea rows="23" id="newkeywords" name="newkeywords" cols="50"><?php print_r ($result);?></textarea></p>
+	  <p>&nbsp;</p>
+	</form>
+<?php
+}}
+
+function sc_get_keywordmarriage(){
+ob_start();
+	$bulkpage = new keywordmarriage;
+	$bulkpage->sc_keywordmarriage();
+$return = ob_get_contents();
+ob_end_clean();
+return $return;
+}
+
+
+function sc_add_keywordmarriage(){
+	$seotools = '<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><small><a href="http://www.seoautomatic.com/free-tools/keyword-multiplier/" target="_blank">This Keyword Multiplier was provided by SEO Automatic</a></small>';
+	return sc_get_keywordmarriage().$seotools;
+}
+
+add_shortcode('keyword-marriage', 'sc_add_keywordmarriage');
+
+
+//Landing Page Determinator
+function sc_get_lpd(){
+ob_start();
+    require_once(ABSPATH.'wp-content/plugins/seo-automatic-seo-tools/lpd/'.'index.php');
+	$bulkpage = new lpd;
+	$bulkpage->sc_lpd();
+$return = ob_get_contents();
+ob_end_clean();
+return $return;
+}
+
+
+function sc_add_lpd(){
+	$seotools = '<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><small><a href="http://www.seoautomatic.com/unique-tools/best-page-determinator/" target="_blank">This Landing Page Determinator was provided by SEO Automatic</a></small>';
+	return sc_get_lpd().$seotools;
+}
+
+add_shortcode('lpd-tool', 'sc_add_lpd');
+
+add_action('admin_menu', 'seo_tools_admin', 1);
+
+function seo_tools_admin() { // Add the menu
+	global $menu;
+	foreach ($menu as $i) {
+		$key = array_search('toplevel_page_seo-automatic-options', $i);
+		if ($key != '') {
+			$menu_added = true;
+		}
+	}
+	if ($menu_added) {
+	} else {
+		add_menu_page('SEO Automatic by Search Commander, Inc.', 'SEO Automatic', 'activate_plugins', 'seo-automatic-options', 'seo_tools_home_page',plugins_url() . '/seo-automatic-seo-tools/images/favicon.png');
+		add_submenu_page('seo-automatic-options', 'SEO Tools Admin', 'Admin', 'activate_plugins', 'seo-automatic-options', 'seo_tools_home_page');
+	}
+	add_submenu_page('seo-automatic-options', 'SEO Tools', 'SEO Tools', 'activate_plugins', dirname(__FILE__) . '/settings.php', 'seo_tools_settings_page');
+	add_submenu_page(null, '', ' - add tool pages', 'activate_plugins', dirname(__FILE__) . '/add-tool-pages.php');
+}
+function seo_tools_home_page(){
+	include('home.php');
+}
+function seo_tools_settings_page(){
+	include('settings.php');
+}
+
+function seo_tools_set_linkback() {
+	if (!get_option('seo_tools_linkback_url')) {
+		$url = get_option('siteurl');
+		update_option('seo_tools_linkback_url', $url);
+	}
+	if (!get_option('seo_tools_linkback_on')) {
+		update_option('seo_tools_linkback_on', 'on');
+	}
+	if (!get_option('seo_tools_linkback_text')) {
+		update_option('seo_tools_linkback_text', 'add RSS feeds to any website');
+	}
+}
+
+add_action('admin_menu', 'seo_tools_set_linkback');
+?>
